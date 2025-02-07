b0b5dc0033d5c63f6fe098c61e33d227cca47c9f
linus@windows <linus@windows.de>
2025-02-07 16:38:29 +0100

    added function from reddit to find gameObject by file id

--- Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
+++ Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
@@ -2,19 +2,66 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class DiffParser : MonoBehaviour
 {
+    public GameObject gameObject;
     private string _namePrefix = "+  m_Name:";
     private string _pathToDiff = "saved_diff.txt";
 
     private void Start()
     {
-        Debug.Log(ExtractDiffObjs(_pathToDiff));
-        var go = GetDiffGameObjects(ExtractDiffObjs(_pathToDiff));
+        // Debug.Log(ExtractDiffObjs(_pathToDiff));
+        gameObject = GetGameObjectFromFileID(198659496);
+        Debug.Log(gameObject.name);
     }
+
+    private static GameObject GetGameObjectFromFileID(long fileID) // also called local identifier
+    {
+        GameObject resultGo = null;
+        var gameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+        // Test every gameobjects
+        foreach (var go in gameObjects)
+        {
+#if UNITY_EDITOR
+            PropertyInfo inspectorModeInfo =
+                typeof(UnityEditor.SerializedObject).GetProperty("inspectorMode",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+            UnityEditor.SerializedObject serializedObject = new UnityEditor.SerializedObject(go);
+            inspectorModeInfo.SetValue(serializedObject, UnityEditor.InspectorMode.Debug, null);
+            UnityEditor.SerializedProperty localIdProp = serializedObject.FindProperty("m_LocalIdentfierInFile");
+#endif
+            if (localIdProp.longValue == fileID)
+            {
+                resultGo = go;
+                return resultGo;
+            }
+        }
+
+        Debug.LogError($"Not found");
+        return null;
+    }
+    
+
+    /*private GameObject GetObjByFileID()
+    {
+        PropertyInfo inspectorModeInfo =
+            typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+  
+        
+        SerializedObject serializedObject = new SerializedObject(go);
+        inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+
+        SerializedProperty localIdProp =
+            serializedObject.FindProperty("m_LocalIdentfierInFile");   //note the misspelling!
+  
+        int localId = localIdProp.intValue;
+
+    }*/
 
     private string[] ExtractDiffObjs(string path)
     {
@@ -29,17 +76,10 @@ public class DiffParser : MonoBehaviour
             print(line);
         }
 
-        /*
-        foreach (var line in linesWhereDiffStarts)
-        {
-            print(line);
-        }
-        */
-
         return gameObjectNamesInDiff;
     }
 
-    private GameObject[] GetDiffGameObjects(string[] linesWhereDiffStarts)
+    private GameObject[] GetDiffGameObjectsByName(string[] linesWhereDiffStarts)
     {
         List<string> filteredNames = new();
         foreach (var line in linesWhereDiffStarts)
