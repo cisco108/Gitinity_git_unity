8bfbbdf6ca1fe467d029ae103ab4c00a16e9dc95
linus@windows <linus@windows.de>
2025-02-07 15:28:48 +0100

    read and filter diff object from scene yaml

--- Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
+++ Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
@@ -1,22 +1,47 @@
 using System;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DiffParser : MonoBehaviour
 {
     private string _pathToDiff = "saved_diff.txt";
 
-    private void Start()
+    private void OnValidate()
     {
         Debug.Log(ExtractDiffObjs());
     }
 
-    private string ExtractDiffObjs()
+    private string[] ExtractDiffObjs()
     {
         if(!File.Exists(_pathToDiff))
             Debug.LogError($"Cant find file {_pathToDiff}");
+        string[] linesWhereDiffStarts = File.ReadLines(_pathToDiff).
+            Where(line => line.StartsWith("+---")).ToArray();
 
+        string[] gameObjectNamesInDiff =  File.ReadLines(_pathToDiff).
+            Where(line => line.StartsWith("+  m_Name:")).ToArray();
         
-        return File.ReadAllText(_pathToDiff);
+        foreach (var line in gameObjectNamesInDiff)
+        {
+            print(line);
+        }
+        foreach (var line in linesWhereDiffStarts)
+        {
+            print(line);
+        }
+
+        return linesWhereDiffStarts;
+    }
+
+    private GameObject[] GetDiffGameObjects(string[] linesWhereDiffStarts)
+    {
+        foreach (var line in linesWhereDiffStarts)
+        {
+           // GameObject go = GameObject.Find() 
+        }
+
+        return null;
     }
 }
