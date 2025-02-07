7a831a98bc9e0fc68cbd1e300d2fd52afec67026
linus@windows <linus@windows.de>
2025-02-07 16:11:07 +0100

    created list with names of objects in diff

--- Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
+++ Assets/Scripts/Improve_Scene_Merge/DiffParser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Unity.VisualScripting;
@@ -6,40 +7,52 @@ using UnityEngine;
 
 public class DiffParser : MonoBehaviour
 {
+    private string _namePrefix = "+  m_Name:";
     private string _pathToDiff = "saved_diff.txt";
 
-    private void OnValidate()
+    private void Start()
     {
-        Debug.Log(ExtractDiffObjs());
+        Debug.Log(ExtractDiffObjs(_pathToDiff));
+        var go = GetDiffGameObjects(ExtractDiffObjs(_pathToDiff));
     }
 
-    private string[] ExtractDiffObjs()
+    private string[] ExtractDiffObjs(string path)
     {
-        if(!File.Exists(_pathToDiff))
-            Debug.LogError($"Cant find file {_pathToDiff}");
-        string[] linesWhereDiffStarts = File.ReadLines(_pathToDiff).
-            Where(line => line.StartsWith("+---")).ToArray();
+        if (!File.Exists(path))
+            Debug.LogError($"Cant find file {path}");
+        string[] linesWhereDiffStarts = File.ReadLines(path).Where(line => line.StartsWith("+---")).ToArray();
 
-        string[] gameObjectNamesInDiff =  File.ReadLines(_pathToDiff).
-            Where(line => line.StartsWith("+  m_Name:")).ToArray();
-        
+        string[] gameObjectNamesInDiff = File.ReadLines(path).Where(line => line.StartsWith("+  m_Name:")).ToArray();
+
         foreach (var line in gameObjectNamesInDiff)
         {
             print(line);
         }
+
+        /*
         foreach (var line in linesWhereDiffStarts)
         {
             print(line);
         }
+        */
 
-        return linesWhereDiffStarts;
+        return gameObjectNamesInDiff;
     }
 
     private GameObject[] GetDiffGameObjects(string[] linesWhereDiffStarts)
     {
+        List<string> filteredNames = new();
         foreach (var line in linesWhereDiffStarts)
         {
-           // GameObject go = GameObject.Find() 
+            if (line.StartsWith(_namePrefix))
+                filteredNames.Add(line.Remove(0, _namePrefix.Length));
+            else
+                Debug.LogWarning($"no name prefix in {line}");
+        }
+
+        foreach (var name in filteredNames)
+        {
+            Debug.Log($"filtered name: {name}");
         }
 
         return null;
