e25f9cb4c94537a3a69b6f21cce2e552885f4f50
linus@windows <linus@windows.de>
2025-02-07 20:06:29 +0100

    clean up

--- Assets/Scripts/Improve_Scene_Merge/DiffGameObjectExtractor.cs
+++ Assets/Scripts/Improve_Scene_Merge/DiffGameObjectExtractor.cs
@@ -1,22 +1,16 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
-public class DiffParser : MonoBehaviour
+public class DiffGameObjectExtractor : MonoBehaviour
 {
     public GameObject gameObject;
     private const string NamePrefix = "+  m_Name:";
     private const string GameObjectPrefix = "+--- !u!1 ";
-
     private string _pathToDiff = "saved_diff.txt";
 
-
-    private const int LengthOfFileID = 9;
 
     private void Start()
     {
@@ -38,6 +32,7 @@ public class DiffParser : MonoBehaviour
         return diffGameObjects;
     }
 
+    //https://www.reddit.com/r/unity/comments/q2jw1u/how_to_find_an_object_in_a_scene_using_its/
     private static GameObject GetGameObjectFromFileID(long fileID) // also called local identifier
     {
         GameObject resultGo = null;
@@ -88,38 +83,4 @@ public class DiffParser : MonoBehaviour
         return diffObjIDs;
     }
 
-    private string[] ExtractDiffObjsNames(string path)
-    {
-        if (!File.Exists(path))
-            Debug.LogError($"Cant find file {path}");
-        string[] linesWhereDiffStarts = File.ReadLines(path).Where(line => line.StartsWith("+---")).ToArray();
-
-        string[] gameObjectNamesInDiff = File.ReadLines(path).Where(line => line.StartsWith("+  m_Name:")).ToArray();
-
-        foreach (var line in gameObjectNamesInDiff)
-        {
-            print(line);
-        }
-
-        return gameObjectNamesInDiff;
-    }
-
-    private GameObject[] GetDiffGameObjectsByName(string[] linesWhereDiffStarts)
-    {
-        List<string> filteredNames = new();
-        foreach (var line in linesWhereDiffStarts)
-        {
-            if (line.StartsWith(NamePrefix))
-                filteredNames.Add(line.Remove(0, NamePrefix.Length));
-            else
-                Debug.LogWarning($"no name prefix in {line}");
-        }
-
-        foreach (var name in filteredNames)
-        {
-            Debug.Log($"filtered name: {name}");
-        }
-
-        return null;
-    }
 }
