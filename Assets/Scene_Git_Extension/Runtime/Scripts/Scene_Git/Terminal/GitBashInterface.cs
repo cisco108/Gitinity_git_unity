74e4c6a46406479fef65908baae2120f36418c1e
linus@windows <linus@windows.de>
2025-02-08 23:09:31 +0100

    added .sh as parameter not working yet

--- Assets/Scene_Git_Extension/Runtime/Scripts/Scene_Git/Terminal/GitBashInterface.cs
+++ Assets/Scene_Git_Extension/Runtime/Scripts/Scene_Git/Terminal/GitBashInterface.cs
@@ -7,14 +7,17 @@ using Debug = UnityEngine.Debug;
 public class GitBashInterface : ITerminalInterface
 {
     private const string PathToBashExe = @"C:\Program Files\Git";
+
+    private const string PathToSh =
+        "Assets/Scene_Git_Extension/Runtime/Scripts/Scene_Git/Terminal/ShellScripts/get_diff.sh";
     public void Execute()
     {
         try
         {
             using (Process gitProcess = new Process())
             {
-                gitProcess.StartInfo.FileName = PathToBashExe;
-                gitProcess.Start();
+                // gitProcess.StartInfo.FileName = PathToBashExe;
+                Process.Start(PathToBashExe, PathToSh);
 
             }
         }
