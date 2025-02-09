7913b17991a04ad63ab6ea0b7ab8a8d42ecb98e4
linus@windows <linus@windows.de>
2025-02-09 16:24:18 +0100

    added scene path to command builder

--- Assets/Scene_Git_Extension/Runtime/Scripts/Scene_Git/Terminal/GitBashCommandBuilder.cs
+++ Assets/Scene_Git_Extension/Runtime/Scripts/Scene_Git/Terminal/GitBashCommandBuilder.cs
@@ -15,10 +15,17 @@ public class GitBashCommandBuilder : ICommandBuilder
         return GitCommands.rev_parse + branch;
     }
 
+    /// <summary>
+    /// Shortens the lenght of hashes to 12 characters.
+    /// Full 40 character hashes lead to wrong execution.
+    /// </summary>
+    /// <param name="mergeBase"></param>
+    /// <param name="revParse"></param>
+    /// <returns></returns>
     public string GetDiff(string mergeBase, string revParse)
     {
         mergeBase = mergeBase.Remove(mergeBase.Length - 28, 28);
         revParse = revParse.Remove(revParse.Length - 28, 28);
-        return GitCommands.diff + mergeBase + " " + revParse + " ";
+        return GitCommands.diff + mergeBase + " " + revParse + " Assets/Scenes";
     }
 }
