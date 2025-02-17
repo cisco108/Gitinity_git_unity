1c0e261f667b649f5315c579e17266ddc155a3a4
linus@windows <linus@windows.de>
2025-02-17 11:05:52 +0100

    made real branches selectable from gui

--- Runtime/Scripts/git_Un/Terminal/GitCommands.cs
+++ Runtime/Scripts/git_Un/Terminal/GitCommands.cs
@@ -16,6 +16,7 @@ public static class GitCommands
     public static string diff = "git diff ";
     public static string subtree_split_prefix = "git subtree split --prefix="; //no space intentional
     public static string push = "git push origin ";
+    public static string branch = "git branch ";
 
 
 }
