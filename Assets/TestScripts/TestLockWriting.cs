f92a4e812ffc1a9f7ff14b0d8471ec46b04473f5
linus@windows <linus@windows.de>
2025-03-29 09:53:44 +0100

    Writing json works.

---
+++ Assets/TestScripts/TestLockWriting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TestLockWriting : MonoBehaviour
+{
+    private TheLock _theLock = new TheLock();
+
+    void Start()
+    {
+        _theLock.WriteLocking();
+    }
+}
---
+++ Assets/git_Un/Runtime/Scripts/git_Un/FileLocking/TheLock.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class TheLock
+{
+    public void WriteLocking()
+    {
+        var lockInfo = new LockInfo("this file man");
+        JsonSerializer serializer = new JsonSerializer();
+        serializer.NullValueHandling = NullValueHandling.Ignore;
+
+        string path = Directory.GetCurrentDirectory();
+        using StreamWriter sw = new StreamWriter( path + @"\json.json");
+        using JsonWriter writer = new JsonTextWriter(sw);
+        serializer.Serialize(writer, lockInfo);
+    }
+}
+
+public class LockInfo
+{
+    public LockInfo(string fileToLock)
+    {
+        lockedFile = fileToLock;
+    }
+
+    public string lockedFile;
+    public int someInt = 23;
+    public int[] arr = new[] { 2, 34, 53, 55, 32 };
+}
