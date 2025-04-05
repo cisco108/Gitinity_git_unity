f7cded11ab00c6871f68956b794047731eceafa6
linus@windows <linus@windows.de>
2025-04-05 19:23:13 +0200

    Implemented local unlocking and locking of multiple files.

--- Runtime/Scripts/git_Un/FileLocking/TheLock.cs
+++ Runtime/Scripts/git_Un/FileLocking/TheLock.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -7,51 +9,101 @@ public class TheLock
     private JsonSerializer serializer = new JsonSerializer();
     private string path = GlobalRefs.filePaths.lockedProtocolFile;
 
-    public void WriteLocking(string file)
+    /*public void WriteLocking(string file)
     {
         var lockInfo = new LockInfo(
-            file, 
+            file,
             // GlobalRefs.filePaths.fileToLockName,
             GlobalRefs.filePaths.userEmail);
 
         using StreamWriter sw = new StreamWriter(path);
         using JsonWriter writer = new JsonTextWriter(sw);
         serializer.Serialize(writer, lockInfo);
+    }*/
+
+    public void WriteLocking(string fileToLock)
+    {
+        LockInfo lockInfo;
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            lockInfo = JsonConvert.DeserializeObject<LockInfo>(json);
+        }
+        else
+        {
+            lockInfo = new LockInfo();
+        }
+
+        int hash = fileToLock.GetHashCode();
+        try
+        {
+            lockInfo.lockedFiles.Add(hash, (fileToLock, GlobalRefs.filePaths.userEmail));
+        }
+        catch
+        {
+            Debug.LogWarning("This file is already locked!");
+            return;
+        }
+
+        using StreamWriter sw = new StreamWriter(path);
+        using JsonWriter writer = new JsonTextWriter(sw);
+        serializer.Serialize(writer, lockInfo);
+        Debug.Log($"{fileToLock} was just locked by {GlobalRefs.filePaths.userEmail}");
     }
 
-    public string ReadLockInfo() // Not really used, just kept it for now.
+    public void WriteUnlocking(string fileToUnlock)
     {
+        if (!File.Exists(path))
+        {
+            Debug.Log("No file has been locked, so unlock not possible.");
+            return;
+        }
+
         string json = File.ReadAllText(path);
-        var r =DeserializeFileLockInfo(json);
-        return $"{r.lockedFile} {r.personWhoLocked}";
+        LockInfo lockInfo = JsonConvert.DeserializeObject<LockInfo>(json);
+        int hash = fileToUnlock.GetHashCode();
+        if (lockInfo.lockedFiles.ContainsKey(hash))
+        {
+            lockInfo.lockedFiles.Remove(hash);
+
+            using StreamWriter sw = new StreamWriter(path);
+            using JsonWriter writer = new JsonTextWriter(sw);
+            serializer.Serialize(writer, lockInfo);
+            Debug.Log($"The file {fileToUnlock} has been unlocked (locally).");
+            return;
+        }
+        Debug.Log($"{fileToUnlock} was not locked.");
     }
+
 
     /// <summary>
     /// Converts the json to a LockInfo object and returns
     /// the lockedFile property.
     /// </summary>
     /// <returns>lockedFile</returns>
-    public (string lockedFile, string personWhoLocked) DeserializeFileLockInfo(string json)
+    public bool IsFileLocked(string json, string fileToCheck, out string lockerEmail)
     {
+        lockerEmail = null;
         var lockInfo = JsonConvert.DeserializeObject<LockInfo>(json);
         if (lockInfo == null)
         {
             Debug.LogError("Deserialization failed.");
-            return (null, null);
+            return false;
         }
-        return (lockInfo.lockedFile, lockInfo.lockerEmail);
+
+        int hash = fileToCheck.GetHashCode();
+        if (lockInfo.lockedFiles.ContainsKey(hash))
+        {
+            var obj = lockInfo.lockedFiles[hash];
+            lockerEmail = obj.Item2;
+            return true;
+        }
+
+        return false;
     }
 }
 
 public class LockInfo
 {
-    public LockInfo(){}
-    public LockInfo(string fileToLock, string lockerMail)
-    {
-        lockedFile = fileToLock;
-        lockerEmail = lockerMail;
-    }
-
-    public string lockedFile;
-    public string lockerEmail;
+    public Dictionary<int, (string, string)> lockedFiles = new();
 }
