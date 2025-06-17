f0a8cb82385d0a7cda8d9af722642f6f28619608
... <thebot@icloud.com>
2025-06-17 23:31:53 +0200

    Cleanup. Ready for folder level rules.

---
+++ Runtime/Scripts/git_Un/AssetVCS/AssetValidationSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AssetValidationSettings", menuName = "Asset VCS/Validation Settings", order = 0)]
+public class AssetValidationSettings : ScriptableObject
+{
+    public int maxVertexCount = 100_000;
+    public float minScaleFactor = 0.0001f;
+    public int minImageWidth = 2;
+    public int minImageHeight = 2;
+    public int minAudioSampleRate = 1;
+}
