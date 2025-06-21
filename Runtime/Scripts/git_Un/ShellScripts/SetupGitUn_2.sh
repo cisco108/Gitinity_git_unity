b4815cfd176bb467bc0ac37f1a1fe0e0ff91e867
... <thebot@icloud.com>
2025-06-21 19:01:32 +0200

    Removed auto setup of git hook for now.

--- Runtime/Scripts/git_Un/ShellScripts/SetupGitUn_2.sh
+++ Runtime/Scripts/git_Un/ShellScripts/SetupGitUn_2.sh
@@ -3,25 +3,7 @@
 echo "true" >> .allow_commit
 
 echo ".allow_commit" >> .gitignore
-
-mv .git/hooks/pre-commit.sample .git/hooks/pre-commit
-
-cat << 'EOF' > .git/hooks/pre-commit
-#!/bin/sh
-
-echo "hello from pre-commit"
-
-A=$(cat .allow_commit 2>/dev/null)
-echo "allowed: $A"
-
-if [ "$A" != "true" ]; then
-    echo "□~]~L Commit blocked: .allow_commit is not 'true'"
-    exit 1
-fi
-
-echo "□~\~E Commit allowed"
-EOF
-        
+      
 git add .gitignore
 
 git commit -m 'added gitignore' 
